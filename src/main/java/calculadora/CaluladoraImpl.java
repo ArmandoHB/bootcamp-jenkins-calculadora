39529ae3fb0f475a9d20749bd1a664e22b8dff53
Hernández Barraza Mario Armando <E13486@banxico.org.mx>
2024-11-01 16:51:10 -0600

    |||

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,14 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-            return i2+i1;
+		if(op=='+') return i1+i2;
+		if(op=='-') return i1-i2;
+		if(op=='*') return i1*i2;
+		if(op=='/') return i1/i2;
+           return -1;
         }
+
+	
+
 
 }
